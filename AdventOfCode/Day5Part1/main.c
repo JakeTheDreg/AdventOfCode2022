838fd614634507976e6258e815f895638f2196a4
JakeTheDreg <91640879+JakeTheDreg@users.noreply.github.com>
2022-12-22 18:47:35 -0500

    Day5Part1 Complete

--- AdventOfCode/Day5Part1/main.c
+++ AdventOfCode/Day5Part1/main.c
@@ -8,8 +8,6 @@
 
 char *bigs[MAX] = {"DBJV", "PVBWRDF", "RGFLDCWQ", "WJPMLNDB", "HNBPCSQ", "RDBSNG", "ZBPMQFSH", "WLF", "SVFMR"};
 
-int bigcount = 0;
-
 char** initStacks(){
     char** stacks = malloc(sizeof(char*) * LENGTH);
     for(int i = 0; i<LENGTH; i++){
@@ -28,19 +26,11 @@ void freeStacks(char** stacks){
 }
 
 void moveOne(char **src, char **dst){
-    bigcount++;
     int srcIndex = strlen(*src)-1;
     int dstIndex = strlen(*dst);
     (*dst)[dstIndex] = (*src)[srcIndex];
     (*dst)[dstIndex+1]='\0';
     (*src)[srcIndex]='\0';
-
-    for(int i = 0; i < srcIndex; i++){
-        if((*src)[i] == '0') exit(bigcount);
-    }
-    for(int i = 0; i < (dstIndex+1); i++){
-        if((*src)[i] == '0') exit(bigcount);
-    }
 }
 
 void moveCrates(char *stacks[], int num, int src, int dst){
