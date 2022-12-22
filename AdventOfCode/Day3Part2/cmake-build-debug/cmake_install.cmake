685fb326c14bb8e97aae17a62fbe1daa55caa72c
JakeTheDreg <91640879+JakeTheDreg@users.noreply.github.com>
2022-12-21 23:16:18 -0500

    Add files via upload

--- AdventOfCode/Day3Part2/cmake-build-debug/cmake_install.cmake
+++ AdventOfCode/Day3Part2/cmake-build-debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/jrbra/CLionProjects/AdventOfCode/AdventOfCode/Day3
+# Install script for directory: C:/Users/jrbra/CLionProjects/AdventOfCode/AdventOfCode/Day3Part2
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/Day3")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/Day3Part2")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/jrbra/CLionProjects/AdventOfCode/AdventOfCode/Day3/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/jrbra/CLionProjects/AdventOfCode/AdventOfCode/Day3Part2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
