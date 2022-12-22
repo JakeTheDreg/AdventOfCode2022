227c72c760183364ab8d98b3d07109284fe422bc
JakeTheDreg <91640879+JakeTheDreg@users.noreply.github.com>
2022-12-22 17:28:52 -0500

    Day5 Broken

--- AdventOfCode/Day5Part1/main.c
+++ AdventOfCode/Day5Part1/main.c
@@ -1,8 +1,47 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-#define MAX 56
+#define MAX 64
 #define WIDTH 20
+#define LENGTH 9
+
+char *bigs[MAX] = {"DBJV", "PVBWRDF", "RGFLDCWQ", "WJPMLNDB", "HNBPCSQ", "RDBSNG", "ZBPMQFSH", "WLF", "SVFMR"};
+
+int bigcount = 0;
+
+char** initStacks(){
+    char** stacks = malloc(sizeof(char*) * LENGTH);
+    for(int i = 0; i<LENGTH; i++){
+        char* temp = calloc(MAX, sizeof(char));
+        strcpy(temp, bigs[i]);
+        stacks[i] = temp;
+    }
+    return stacks;
+}
+
+void moveOne(char **src, char **dst){
+    bigcount++;
+    int srcIndex = strlen(*src)-1;
+    int dstIndex = strlen(*dst);
+    (*dst)[dstIndex] = (*src)[srcIndex];
+    (*dst)[dstIndex+1]='\0';
+    (*src)[srcIndex]='\0';
+
+    for(int i = 0; i < srcIndex; i++){
+        if((*src)[i] == '0') exit(bigcount);
+    }
+    for(int i = 0; i < (dstIndex+1); i++){
+        if((*src)[i] == '0') exit(bigcount);
+    }
+}
+
+void moveCrates(char *stacks[], int num, int src, int dst){
+    while(num>0){
+        moveOne(&stacks[src], &stacks[dst]);
+        num--;
+    }
+}
 
 int main() {
     FILE* puzzleFile;
@@ -12,16 +51,7 @@ int main() {
         return -1;
     }
 
-    char stack1[MAX] = "DBJV";
-    char stack2[MAX] = "PVBWRDF";
-    char stack3[MAX] = "RGFLDCWQ";
-    char stack4[MAX] = "WJPMLNDB";
-    char stack5[MAX] = "HNBPCSQ";
-    char stack6[MAX] = "RDBSNG";
-    char stack7[MAX] = "ZBPMQFSH";
-    char stack8[MAX] = "WLF";
-    char stack9[MAX] = "SVFMR";
-    char* stacks[9] = {stack1, stack2, stack3, stack4, stack5, stack6, stack7, stack8, stack9};
+    char** stacks = initStacks();
     char line[WIDTH];
     int num;
     int src;
@@ -29,7 +59,13 @@ int main() {
 
     while(fgets(line, WIDTH, puzzleFile) != NULL){
         sscanf(line, "move %d from %d to %d", &num, &src, &dst);
+        moveCrates(stacks, num, src-1, dst-1);
     }
+    for(int i = 0; i < 9; i++){
+        int this = strlen(stacks[i])-1;
+        printf("%c", stacks[i][this]);
+    }
+
 
     fclose(puzzleFile);
     return 0;
